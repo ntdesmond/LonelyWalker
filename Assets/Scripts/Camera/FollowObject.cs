2611db1616ac3e0d8b258ce800ff1ba74349f51f
Desmond Ainotohi <ntdesmond@gmail.com>
2022-03-04 12:41:42 +0300

    Create sample scene and configure player animation

---
+++ Assets/Scripts/Camera/FollowObject.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class FollowObject : MonoBehaviour
+{
+    public Transform objectToFollow;
+
+    private Vector3 _offset;
+    private float _originalY;
+    
+    private void Awake()
+    {
+        var position = transform.position;
+        _offset = position - objectToFollow.position;
+        _originalY = position.y;
+    }
+
+    private void LateUpdate()
+    {
+        var newPosition = objectToFollow.position + _offset;
+        newPosition.y = _originalY;
+        transform.position = newPosition;
+    }
+}
